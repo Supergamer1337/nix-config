b186216d375e8f7377af3bc702a67285099cea34
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2024-06-29 15:09:13 +0200

    Fixed dconf

--- users/felbjar/desktops/gnome/dconf.nix
+++ users/felbjar/desktops/gnome/dconf.nix
@@ -5,6 +5,39 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
+    "org/gnome/control-center" = {
+      last-panel = "system";
+      window-state = mkTuple [ 980 640 false ];
+    };
+
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [ "Utilities" "YaST" "Pardus" ];
+    };
+
+    "org/gnome/desktop/app-folders/folders/Pardus" = {
+      categories = [ "X-Pardus-Apps" ];
+      name = "X-Pardus-Apps.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = [ "gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.fonts.desktop" "org.gnome.Loupe.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" "vinagre.desktop" ];
+      categories = [ "X-GNOME-Utilities" ];
+      name = "X-GNOME-Utilities.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/YaST" = {
+      categories = [ "X-SuSE-YaST" ];
+      name = "suse-yast.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/input-sources" = {
+      sources = [ (mkTuple [ "xkb" "se+us" ]) ];
+      xkb-options = [ "terminate:ctrl_alt_bksp" ];
+    };
+
     "org/gnome/desktop/background" = {
       picture-uri = "/home/felbjar/.nix-config/resources/wallpapers/candy-river-land.png";
       picture-uri-dark = "/home/felbjar/.nix-config/resources/wallpapers/candy-river-land.png";
@@ -12,18 +45,6 @@ with lib.hm.gvariant;
       secondary-color = "#2f302f";
     };
 
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-      show-battery-percentage = true;
-    };
-
-    "org/gnome/desktop/peripherals/mouse" = {
-      natural-scroll = true;
-    };
-
-    "org/gnome/desktop/peripherals/touchpad" = {
-      two-finger-scrolling-enabled = true;
-    };
 
     "org/gnome/desktop/screensaver" = {
       color-shading-type = "solid";
@@ -34,12 +55,51 @@ with lib.hm.gvariant;
       secondary-color = "#2f302f";
     };
 
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      show-battery-percentage = true;
+      toolkit-accessibility = false;
+    };
+
+    "org/gnome/desktop/peripherals/mouse" = {
+      natural-scroll = true;
+    };
+
+    "org/gnome/desktop/peripherals/touchpad" = {
+      two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/privacy" = {
+      old-files-age = mkUint32 30;
+      recent-files-max-age = -1;
+    };
+
+    "org/gnome/desktop/search-providers" = {
+      sort-order = [ "org.gnome.Contacts.desktop" "org.gnome.Documents.desktop" "org.gnome.Nautilus.desktop" ];
+    };
+
+    "org/gnome/desktop/session" = {
+      idle-delay = mkUint32 300;
+    };
+
     "org/gnome/desktop/wm/keybindings" = {
       close = [ "<Super>w" ];
     };
 
+    "org/gnome/evolution-data-server" = {
+      migrated = true;
+    };
+
     "org/gnome/mutter" = {
       dynamic-workspaces = true;
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      migrated-gtk-settings = true;
+    };
+
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
@@ -49,9 +109,8 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/shell" = {
-      disable-user-extensions = false;
-      enabled-extensions = [ "blur-my-shell@aunetx" "gsconnect@andyholmes.github.io" "gnome-ui-tune@itstime.tech" ];
-      favorite-apps = [ "brave-browser.desktop" "org.gnome.Nautilus.desktop" ];
+      enabled-extensions = [ "blur-my-shell@aunetx" "gnome-ui-tune@itstime.tech" ];
+      favorite-apps = [ "brave-browser.desktop" "Alacritty.desktop" "org.gnome.Nautilus.desktop" ];
       welcome-dialog-last-shown-version = "46.2";
     };
 
@@ -82,9 +141,9 @@ with lib.hm.gvariant;
       sigma = 30;
     };
 
-    "org/gnome/shell/extensions/gsconnect" = {
-      id = "9e2df182-cd23-40c2-8cb3-2d5bd9609824";
-      name = "roctim-nix";
+    "org/gnome/shell/world-clocks" = {
+      locations = [];
     };
+
   };
 }

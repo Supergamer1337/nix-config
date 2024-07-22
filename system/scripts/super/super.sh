4b7b5821d3bcbc876143dc82a8a39bec11da97f3
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2024-07-22 15:52:26 +0200

    Add GC script

--- system/scripts/super/super.sh
+++ system/scripts/super/super.sh
@@ -1,21 +1,34 @@
 NIX_DIR=$HOME/.nix-config
 
-function sync_user() {
+function sync_user {
     home-manager switch --flake $NIX_DIR#user;
 }
 
-function sync_system() {
+function sync_system {
     sudo nixos-rebuild switch --flake $NIX_DIR#system;
 }
 
-function update_flake() {
+function update_flake {
     sudo nix flake update $NIX_DIR;
 }
 
-function upgrade_system() {
+function upgrade_system {
     update_flake
     sync_system
     sync_user
+}
+
+function gc {
+    if [ "$1" = "full" ]; then
+        sudo nix-collect-garbage --delete-old;
+        nix-collect-garbage --delete-old;
+    else
+        sudo nix-collect-garbage --delete-older-than $1;
+        nix-collect-garbage --delete-older-than $1;
+    fi
+
+    # Update the bootloader
+    sudo nixos-rebuild boot --flake $NIX_DIR#system
 }
 
 # Structure of running the correct function
@@ -37,6 +50,18 @@ elif [ "$1" = "update" ]; then
     update_flake
 elif [ "$1" = "upgrade" ]; then
     upgrade_system
+elif [ "$1" = "gc" ]; then
+    if [ "$#" -gt 2 ]; then
+        echo "Warning: The 'gc' command only accepts one argument (collect_older_than)";
+    fi
+
+    if [ "$2" = "full" ]; then
+        gc "full"
+    elif [ "$2" ]; then
+        gc $2
+    else
+        gc "30d"
+    fi        
 else
     echo "No arguments where supplied."
 fi

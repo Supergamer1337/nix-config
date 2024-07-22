690bc5feaac261ab338691c90803bfba649360a5
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2024-07-22 13:24:50 +0200

    Fixed issue with hardware-configuration.nix

--- install.sh
+++ install.sh
@@ -15,23 +15,14 @@ nix-shell -p git --command "git clone https://github.com/SuperGamer1337/nix-conf
 # Generate the hardware configuration for the new system
 sudo nixos-generate-config --show-hardware-config > $NIX_CONFIG_DIR/hardware-configuration.nix
 
-# Termporarily add the hardware configuration to git so that it can be used to build the system
-cd $NIX_CONFIG_DIR
-git add -f hardware-configuration.nix
-cd ~
-
 # Rebuild and switch to the new system 
 sudo nixos-rebuild switch --flake $NIX_CONFIG_DIR#system
 
 # Install and build home-manager configuration
 nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $NIX_CONFIG_DIR#user
 
-# Remove the hardware configuration from git
-cd $NIX_CONFIG_DIR
-git restore --cached hardware-configuration.nix
-cd ~
-
 echo "Installation complete!"
+
 echo "Rebooting in"
 echo "5 seconds..."
 sleep 1

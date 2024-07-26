7d14259b473156cc3857e0b412fcf5c8cc19621f
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2024-07-26 12:14:28 +0200

    Re-added direnv

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -32,6 +32,5 @@
   config = {
     # Enable CUPS to print documents.
     services.printing.enable = true;
-
   };
 }
--- nixos-modules/programs.nix
+++ nixos-modules/programs.nix
@@ -18,5 +18,7 @@
       clean.enable = true;
       clean.extraArgs = "--keep-since 7d --keep 3"; 
     };
+
+    programs.direnv.enable = true;
   };
 }

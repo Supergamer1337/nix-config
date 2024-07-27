c01fe5d1f612acb440df695266de7580f23e6252
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2024-07-27 21:02:19 +0200

    Alacritty ssh fix

--- modules/home/terminal/alacritty.nix
+++ modules/home/terminal/alacritty.nix
@@ -10,5 +10,7 @@
         window.startup_mode = "Maximized";
       };
     };
+    # Fix for alacritty creating weird errors when sshing
+    programs.zsh.shellAliases."ssh" = "TERM=xterm-256color ssh";
   };
 }

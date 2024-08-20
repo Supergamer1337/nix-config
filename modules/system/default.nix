84b1c2e56556e2cebb8cd2b2264eb1695b12c7b9
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2024-08-20 16:05:07 +0200

    Added cachix

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -46,6 +46,14 @@
 
     time.hardwareClockInLocalTime = config.systemSettings.dualBoot.enable;
 
-    nix.settings.auto-optimise-store = true;
+    nix.settings = {
+      auto-optimise-store = true;
+
+      trusted-users = [ config.userSettings.username ];
+
+      # Enable cachix to speed up builds
+      extra-substituters = [ "https://nix-community.cachix.org" ];
+      extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+    };
   };
 }
--- modules/system/programs.nix
+++ modules/system/programs.nix
@@ -7,6 +7,7 @@
     # System level packages
     environment.systemPackages = with pkgs; [
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      cachix
     ];
 
     # Configured programs

6d68be4276dca28b532f96272a355fe2131731f2
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2024-11-08 12:47:18 +0100

    Updated dconf to be correct finally

--- modules/home/desktop/gnome/dconf.nix
+++ modules/home/desktop/gnome/dconf.nix
@@ -1,4 +1,4 @@
-# Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
+# Generated via dconf2nix: https://github.com/nix-commmunity/dconf2nix
 { lib, ... }:
 
 with lib.hm.gvariant;
@@ -51,16 +51,52 @@ with lib.hm.gvariant;
       application-id = "brave-browser.desktop";
     };
 
+    "org/gnome/desktop/notifications/application/com-github-eneshecan-whatsappforlinux" = {
+      application-id = "com.github.eneshecan.WhatsAppForLinux.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/dbeaver" = {
+      application-id = "dbeaver.desktop";
+    };
+
     "org/gnome/desktop/notifications/application/gnome-power-panel" = {
       application-id = "gnome-power-panel.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/jetbrains-rider-1beaeaa6-859a-41f1-a074-3321c6bac056" = {
+      application-id = "jetbrains-rider-1beaeaa6-859a-41f1-a074-3321c6bac056.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-gnome-clocks" = {
+      application-id = "org.gnome.clocks.desktop";
     };
 
     "org/gnome/desktop/notifications/application/org-gnome-evolution-alarm-notify" = {
       application-id = "org.gnome.Evolution-alarm-notify.desktop";
     };
 
+    "org/gnome/desktop/notifications/application/org-gnome-fileroller" = {
+      application-id = "org.gnome.FileRoller.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/pycharm-professional" = {
+      application-id = "pycharm-professional.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/rider" = {
+      application-id = "rider.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/slack" = {
+      application-id = "slack.desktop";
+    };
+
     "org/gnome/desktop/notifications/application/vesktop" = {
       application-id = "vesktop.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/zen" = {
+      application-id = "zen.desktop";
     };
 
     "org/gnome/desktop/peripherals/keyboard" = {
@@ -69,6 +105,7 @@ with lib.hm.gvariant;
 
     "org/gnome/desktop/peripherals/mouse" = {
       accel-profile = "flat";
+      natural-scroll = false;
     };
 
     "org/gnome/desktop/peripherals/touchpad" = {
@@ -135,8 +172,9 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/shell" = {
-      enabled-extensions = [ "blur-my-shell@aunetx" "gnome-ui-tune@itstime.tech" "appindicatorsupport@rgcjonas.gmail.com" ];
-      favorite-apps = [ "zen-browser.desktop" "Alacritty.desktop" "org.gnome.Nautilus.desktop" ];
+      enabled-extensions = [ "blur-my-shell@aunetx" "gnome-ui-tune@itstime.tech" "appindicatorsupport@rgcjonas.gmail.com" "tailscale@joaophi.github.com" ];
+      favorite-apps = [ "Alacritty.desktop" "zen.desktop" "org.gnome.Nautilus.desktop" ];
+      last-selected-power-profile = "performance";
       welcome-dialog-last-shown-version = "46.2";
     };
 
@@ -145,25 +183,21 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/shell/extensions/blur-my-shell/appfolder" = {
-      brightness = 0.6;
       sigma = 30;
     };
 
     "org/gnome/shell/extensions/blur-my-shell/dash-to-dock" = {
       blur = true;
-      brightness = 0.6;
       sigma = 30;
       static-blur = true;
       style-dash-to-dock = 0;
     };
 
     "org/gnome/shell/extensions/blur-my-shell/panel" = {
-      brightness = 0.6;
       sigma = 30;
     };
 
     "org/gnome/shell/extensions/blur-my-shell/window-list" = {
-      brightness = 0.6;
       sigma = 30;
     };
 
@@ -172,7 +206,7 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/shell/world-clocks" = {
-      locations = [];
+      locations = mkArray "v" [];
     };
 
     "org/gnome/software" = {
@@ -197,6 +231,5 @@ with lib.hm.gvariant;
       view-type = "list";
       window-size = mkTuple [ 859 372 ];
     };
-
   };
 }

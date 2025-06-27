bb72b6a82c5f169327766402e2265f0371aff20a
Felix Bjerhem Aronsson <felix.b.aronsson@gmail.com>
2025-06-27 08:29:02 +0200

    Added specialisation for work

--- hosts/roctim-nix/configuration.nix
+++ hosts/roctim-nix/configuration.nix
@@ -12,33 +12,7 @@
     name = "roctim-nix"; 
     theme = "catppuccin";
 
-    desktop.hyprland = {
-      enable = true;
-
-      monitors = [
-        "eDP-1, 1920x1080@60, 0x1080, 1"
-        "DP-3, 1920x1080@60, 0x0, 1"
-        "DP-4, 1920x1080@60, 1920x0, 1"
-      ];
-
-      workspaces = [
-        ### LAPTOP MONITOR ###
-        "7, monitor:eDP-1,persistent:true,default:true"
-        "8, monitor:eDP-1,persistent:true"
-        "9, monitor:eDP-1,persistent:true"
-        "10, monitor:eDP-1,persistent:true"
-
-        ### MIDDLE WORK MONITOR ###
-        "1,monitor:DP-3,persistent:true,default:true"
-        "2,monitor:DP-3,persistent:true"
-        "3,monitor:DP-3,persistent:true"
-
-        ### RIGHT WORK MONITOR ###
-        "4,monitor:DP-4,persistent:true,default:true"
-        "5,monitor:DP-4,persistent:true"
-        "6,monitor:DP-4,persistent:true"
-      ];
-    };
+    desktop.hyprland.enable = true;
 
     hardware = {
       cpu.vendor = "intel";
@@ -69,6 +43,37 @@
     dev.enable = true;
     gaming.enable = false;
     personal.enable = true;
+  };
+
+  # Specialisations
+  specialisation = {
+    work-setup.configuration = {
+      systemSettings.desktop.hyprland = {
+        monitors = [
+          "eDP-1, 1920x1080@60, 0x1080, 1"
+          "DP-3, 1920x1080@60, 0x0, 1"
+          "DP-4, 1920x1080@60, 1920x0, 1"
+        ];
+
+        workspaces = [
+          ### LAPTOP MONITOR ###
+          "7, monitor:eDP-1,persistent:true,default:true"
+          "8, monitor:eDP-1,persistent:true"
+          "9, monitor:eDP-1,persistent:true"
+          "10, monitor:eDP-1,persistent:true"
+
+          ### MIDDLE WORK MONITOR ###
+          "1,monitor:DP-3,persistent:true,default:true"
+          "2,monitor:DP-3,persistent:true"
+          "3,monitor:DP-3,persistent:true"
+
+          ### RIGHT WORK MONITOR ###
+          "4,monitor:DP-4,persistent:true,default:true"
+          "5,monitor:DP-4,persistent:true"
+          "6,monitor:DP-4,persistent:true"
+        ];
+      };
+    };
   };
 
   services.flatpak.enable = true;
